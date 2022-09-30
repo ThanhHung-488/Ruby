dd13e5f89632e1aedeabd4d403110fb968560c5f
ThanhHung-488 <thanhhung14072001@gmail.com>
2022-09-30 11:12:57 +0700

    submit

--- BT5_oop/oop.rb
+++ BT5_oop/oop.rb
@@ -1,31 +1,31 @@
 #Yêu cầu 1: Xây dựng các lớp CongNhan, KySu, NhanVien kế thừa từ lớp CanBo.
 class CanBo
     #Họ tên, tuổi, giới tính(name, nữ, khác), địa chỉ.
-    attr_accessor :ten, :tuoi, :gioitinh, :diachi
-    def initialize ten, tuoi, gioitinh, diachi
-        @ten = ten
-        @tuoi = tuoi
-        @gioitinh = gioitinh
-        @diachi = diachi
+    attr_accessor :ten226, :tuoi226, :gioitinh226, :diachi226
+    def initialize ten226, tuoi226, gioitinh226, diachi226
+        @ten226 = ten226
+        @tuoi226 = tuoi226
+        @gioitinh226 = gioitinh226
+        @diachi226 = diachi226
     end
     def display 
-        puts "Ten: #{@name}"
-        puts "Tuoi: #{@tuoi}"
-        puts "Gioi tinh: #{@gioitinh}"
-        puts "Dia chi: #{@diachi}"
+        puts "Ten: #{@name226}"
+        puts "Tuoi: #{@tuoi226}"
+        puts "Gioi tinh: #{@gioitinh226}"
+        puts "Dia chi: #{@diachi226}"
     end
 
 end
  #Tạo lớp Công nhân kế thừa từ can bo
  class CongNhan < CanBo
-    attr_accessor :bacdaotao
-    def initialize ten, tuoi, gioitinh, diachi, bac1
-        super(ten, tuoi, gioitinh, diachi, bacdaotao)
-        @bacdaotao = bac
+    attr_accessor :bacdaotao226
+    def initialize ten226, tuoi226, gioitinh226, diachi226, bac226
+        super(ten226, tuoi226, gioitinh226, diachi226, bacdaotao226)
+        @bacdaotao226 = bac226
     end
     def display
         super()
-        puts("Bac dao tao: #@bacdaotao")
+        puts("Bac dao tao: #@bacdaotao226")
     end
 end
  #Tạo lớp Ky su kế thừa từ can bo
@@ -44,14 +44,14 @@ end
 #Tạo lớp NhanVien kế thừa từ can bo
 class NhanVien < CanBo
     #Cấp nhan vien có thuộc tính riêng: Công việc
-    attr_accessor :congviec
-    def initialize ten, tuoi, gioitinh, diachi, congviec
-        super(ten, tuoi, gioitinh, diachi, congviec)
-        @congviec = cv
+    attr_accessor :congviec226
+    def initialize ten226, tuoi226, gioitinh226, diachi226, congviec226
+        super(ten226, tuoi226, gioitinh226, diachi226, congviec226)
+        @congviec226 = cv226
     end
     def display
         super()
-        puts("Cong viec la: #@congviec")
+        puts("Cong viec la: #@congviec226")
     end
 end
 #Xây dựng lớp QLCB(quản lý cán bộ) 
@@ -60,18 +60,18 @@ class QLCB
     #Thêm mới cán bộ
     def canbo_adding
         puts 'Ten:'
-        ten = gets.chomp.to_s
+        ten226 = gets.chomp.to_s
       
         puts 'Tuoi:'
-        tuoi = gets.chomp.to_s
+        tuoi226 = gets.chomp.to_s
       
         puts 'Gioi tinh :'
-        gioitinh = gets.chomp.to_s
+        gioitinh226 = gets.chomp.to_s
       
         puts 'Dia chi :'
-        diachi = gets.chomp.to_s
+        diachi226 = gets.chomp.to_s
       
-        new_canbo = CanBo.new(ten, tuoi, gioitinh, diachi)
+        new_canbo = CanBo.new(ten226, tuoi226, gioitinh226, diachi226)
       
         $canbo_array.push(new_canbo.to_hash)
         puts "Da them nhan vien moi thanh cong. Bam ENTER de tiep tuc"
