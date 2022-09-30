d96785ac3e734ef41ecaa2d811c9af6ff3b27c66
ThanhHung-488 <thanhhung14072001@gmail.com>
2022-09-30 10:46:11 +0700

    submit

---
+++ BT5_oop/oop.rb
@@ -0,0 +1,64 @@
+#Yêu cầu 1: Xây dựng các lớp CongNhan, KySu, NhanVien kế thừa từ lớp CanBo.
+class CanBo
+    #Họ tên, tuổi, giới tính(name, nữ, khác), địa chỉ.
+    attr_accessor :ten, :tuoi, :gioitinh, :diachi
+    def initialize ten, tuoi, gioitinh, diachi
+        @ten = ten
+        @tuoi = tuoi
+        @gioitinh = gioitinh
+        @diachi = diachi
+    end
+    def display 
+        puts "Ten: #{@name}"
+        puts "Tuoi: #{@tuoi}"
+        puts "Gioi tinh: #{@gioitinh}"
+        puts "Dia chi: #{@diachi}"
+    end
+
+end
+ #Tạo lớp Công nhân kế thừa từ can bo
+ class CongNhan < CanBo
+    attr_accessor :bacdaotao
+    def initialize ten, tuoi, gioitinh, diachi, bac1
+        super(ten, tuoi, gioitinh, diachi, bacdaotao)
+        @bacdaotao = bac
+    end
+    def display
+        super()
+        puts("Bac dao tao: #@bacdaotao")
+    end
+end
+ #Tạo lớp Ky su kế thừa từ can bo
+ class KySu < CanBo
+    #Cấp kỹ sư có thuộc tính riêng: Nghành đào tạo.
+    attr_accessor :nganhdaotao
+    def initialize ten, tuoi, gioitinh, diachi, nganhdaotao
+        super(ten, tuoi, gioitinh, diachi, nganhdaotao)
+        @nganhdaotao = nganh
+    end
+    def display
+        super()
+        puts("Nganh dao tao: #@nganhdaotao")
+    end
+end
+#Tạo lớp NhanVien kế thừa từ can bo
+class NhanVien < CanBo
+    #Cấp nhan vien có thuộc tính riêng: Công việc
+    attr_accessor :congviec
+    def initialize ten, tuoi, gioitinh, diachi, congviec
+        super(ten, tuoi, gioitinh, diachi, congviec)
+        @congviec = cv
+    end
+    def display
+        super()
+        puts("Cong viec la: #@congviec")
+    end
+end
+#Xây dựng lớp QLCB(quản lý cán bộ) 
+class QLCB 
+    attr_accessor :danhsach
+    def initialize
+        @danhsach = Array.new
+        
+    end
+end
