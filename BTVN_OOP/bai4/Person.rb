efaf8df0bea053455e010bba09a15557a8d3cff9
ThanhHung-488 <thanhhung14072001@gmail.com>
2022-10-12 22:45:30 +0700

    submit

---
+++ BTVN_OOP/bai4/Person.rb
@@ -0,0 +1,9 @@
+class Person
+    attr_accessor :name_226, :age_226, :job_226, :passport_226
+    def initialize(name_226, age_226, job_226, passport_226)
+        @name_226 = name_226
+        @age_226 = age_226
+        @job_226 = job_226
+        @passport_226 = passport_226
+    end
+end
